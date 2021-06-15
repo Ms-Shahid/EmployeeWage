14b6549c294b1f91bde7bfeae8afede3f3ecd9d3
Ms-Shahid <ms2406shahid@gmail.com>
2021-06-15 21:33:39 +0530

    New wage calculation

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -3,19 +3,28 @@
 #Display a welcome message
 echo "Welcome to Empcomputation"
 
+#New Updated featur
 # Constant variable
-IS_PRESENT=1
+IS_PRESENT_FULL=1
+IS_PRESENT_HALF=2
 EMP_WAGE_PER_HR=20
 
-samp=$(( RANDOM%2 ))
+samp=$(( RANDOM%3 ))
 
-if [ $samp -eq $IS_PRESENT ]
+if [ $samp -eq $IS_PRESENT_FULL ]
 then
 	empHrs=8
+
+elif [ $samp -eq $IS_PRESENT_HALF ]
+then
+	empHrs=4
+
 else
 	empHrs=0
 fi
 
 salary=$(( empHrs * EMP_WAGE_PER_HR ))
+
+echo "Employee Salary is : "$salary
 
 
