2124f6d23040edd112f9474ad438fe8eff2e9c63
Ms-Shahid <ms2406shahid@gmail.com>
2021-06-15 19:54:32 +0530

    Added a wagecomputations

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -2,15 +2,19 @@
 
 #Display a welcome message
 echo "Welcome to Empcomputation"
+
 # Constant variable
 IS_PRESENT=1
+EMP_WAGE_PER_HR=20
 
 samp=$(( RANDOM%2 ))
 
 if [ $samp -eq $IS_PRESENT ]
 then
-	echo "Employee Absent"
+	empHrs=8
 else
-	echo "Employee Present"
+	empHrs=0
 fi
+salary=$(( empHrs * EMP_WAGE_PER_HR ))
+
 
