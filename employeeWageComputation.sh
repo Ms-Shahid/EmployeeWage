24d294a1698944ee4a3aea44db93adbf98b81c19
Ms-Shahid <ms2406shahid@gmail.com>
2021-06-15 19:21:44 +0530

    Attendance check

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -2,4 +2,15 @@
 
 #Display a welcome message
 echo "Welcome to Empcomputation"
+# Constant variable
+IS_PRESENT=1
+
+samp=$(( RANDOM%2 ))
+
+if [ $samp -eq $IS_PRESENT ]
+then
+	echo "Employee Absent"
+else
+	echo "Employee Present"
+fi
 
